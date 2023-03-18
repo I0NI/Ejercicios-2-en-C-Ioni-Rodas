c48c897e65e9217900b0698d1abea32c5f42eda3
I0NI <sandritamendez56@gmail.com>
2023-03-18 10:20:13 -0600

    Ejercicios #2 c++ Ioni Rodas

---
+++ main.cpp
@@ -0,0 +1,96 @@
+#include <iostream>
+using namespace std;
+//con un numero ingresado por el usuario calcular el factorial y mostrarlo en pantalla.
+//crear la serie fibonacci
+// con un numero ingresado por el usuario mostrar en pantalla si el numero es primo o no.
+//con un numero ingresado por el usuario mostrar por pantalla si el numero es par o no.
+// con un numero mostrado por el usuario mostrar la tabla de multiplicar.
+/*
+int main() {
+    int numero, i;
+    float fact = 1;
+    cout<<"Ingrese un numero: "<<endl;
+    cin>>numero;
+    for(i=1; i<= numero; i++){
+        fact = fact * i;
+    }
+
+    cout <<"El factorial de: "<<numero<<" Es de: "<<fact<<endl;
+    return 0;
+}
+*/
+/*
+int main(){
+    int uno = 0,dos = 1,tres,numero = 10, i;
+    for(i=1; i<= numero; i++){
+        cout<<uno<<" ";
+        tres = uno + dos;
+        uno = dos;
+        dos = tres;
+
+    }
+
+
+
+
+    return 0;
+}
+ */
+/*
+int main(){
+    int numero;
+    cout<<"Ingrese el numero: "; cin>>numero;
+    if (numero == 0){
+        cout<<"Pos es un cero..."<<endl;
+    }
+    else if (numero == 1){
+        cout<<"Es un numero primo."<<endl;
+
+    }
+    else if (numero == 2){
+        cout<<"Es un numero primo."<<endl;
+
+    }
+    else if (numero%2== 0){
+        cout<<"No es un numero primo."<<endl;
+
+    }
+    else {
+        cout<<"Es un numero primo."<<endl;
+
+    }
+
+
+    return 0;
+}
+ */
+/*
+int main(){
+    int numero;
+    cout<<"Ingrese un numero: ";cin>>numero;
+    if(numero%2 == 0){
+        cout<<"Es un numero par."<<endl;
+    }else{
+        cout<<"Es un numero impar."<<endl;
+    }
+
+    return 0;
+}
+ */
+/*
+int main() {
+    int n;
+    do{
+        cout<<"ingrese un numero pa: ";
+        cin>>n;
+    }while((n<1) || (n>10));
+
+    for(int x=1;x<=10;x++){
+        cout<<n<<" * "<<x<<" = "<<n*x<<endl;
+
+    }
+
+
+    return 0;
+}
+ */
